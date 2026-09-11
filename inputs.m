77902db653109e6e0006fe8469b105434ab65dfa
lyntle <lyntle90@gmail.com>
2026-09-11 11:27:31 -0500

    Update inputs.m
    Changes to Inputs. Need to change rho_m to an array.

--- inputs.m
+++ inputs.m
@@ -10,9 +10,13 @@ Do        = 5;       % outer diameter [in]
 t_wall    = 0.315;   % wall thickness [in]
 rho_c     = 0.06141; % explosive density [lbf/in^3]
 rho_m     = 0.2836;  % casing material density, steel [lbf/in^3]
+rho_m = [0.2836; 0.16004 ;0.06900]; % Material Density [lbf/in3] 
+% Material Densities Used are AISI 4340 Steel, Ti-6Al-4V Titanium, 7075-T6
 L_warhead = 39;      % warhead length [in]
 Pd_req    = 0.90;    % required damage probability (90%)
 approach_deg = 0;    % approach angle [deg]: 0 = nose-on, 90 = broadside
+m = 30.8647; % grains (2 grams)
+phi_half = 10; % [degrees] Provided Angle 
 
 %% Impact angles
 phi_vec = [45, 60, 90];  % [deg]
