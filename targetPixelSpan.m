c2f890780c126248ebd9526d6b2c446291277b2d
gabejohnson18 <gabe.johnson5950@ku.edu>
2026-09-11 11:50:15 -0500

    Update targetPixelSpan.m

--- targetPixelSpan.m
+++ targetPixelSpan.m
@@ -1,3 +1,5 @@
+% Gabe Johnson
+% This function takes inputs and outputs the half-angle as seen from distance and the number of pixels the target spans.
 function [epsTarget, numSpanningPixels] = targetPixelSpan(L_target, r, eps_i)
 
     epsTarget = atan((L_target / 2) / r);
