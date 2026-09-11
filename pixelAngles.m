de1bcb425b4662fd525cfe7de4d4e227eb6f0317
Declan Kurtz <98838300+PumkinPa@users.noreply.github.com>
2026-09-11 11:50:20 -0500

    Added my name
    Added author attribution in the function header.

--- pixelAngles.m
+++ pixelAngles.m
@@ -1,4 +1,4 @@
-function eps_i = pixelAngles(numPixels, pixelWidth, focalLength)
+function eps_i = pixelAngles(numPixels, pixelWidth, focalLength) %Declan Kurtz
 
     pixelIndex = (-(numPixels-1)/2 : (numPixels-1)/2);   % symmetric index
     eps_i = atan(pixelIndex .* pixelWidth ./ focalLength); %anglular pixel location in radians
